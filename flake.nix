92794759eb9c20fedd63c81f00037bcb3c98bb92
Hubert Behaghel <behaghel@gmail.com>
2021-07-10 17:37:23 +0100

    [hub@dell-laptop] adding hub's home to dell-laptop
    following guidance from
    https://github.com/nix-community/home-manager/blob/3902a0697c4865dfc74f396ed2ff42596e2dd438/doc/flakes.adoc

--- flake.nix
+++ flake.nix
@@ -3,23 +3,59 @@
 
   inputs.nixpkgs.url = "nixpkgs/nixos-21.05-small";
 
-  outputs = { self, nixpkgs }: {
+  inputs.home-manager.url = "github:nix-community/home-manager/master";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixosConfigurations.dell-laptop = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({pkgs, ...}: {
-          # let 'nixos-version --json' know about git revision of this flake
-          system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-        })
-        (./hosts/default.nix)
-        (./hosts/dell-laptop/default.nix)
-      ];
+  inputs.hub-home.url = "github:behaghel/dotfiles/stow-refactoring";
+  inputs.hub-home.inputs.nixpkgs.follows = "nixpkgs";
+
+  outputs = { self, nixpkgs, home-manager, hub-home }:
+    let
+      # Function to create default (common) system config options
+      defFlakeSystem = baseCfg:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            (./hosts/default.nix)
+            # Add home-manager option to all configs
+            ({ ... }: {
+              imports = [
+                {
+                  # Set the $NIX_PATH entry for nixpkgs. This is necessary in
+                  # this setup with flakes, otherwise commands like `nix-shell
+                  # -p pkgs.htop` will keep using an old version of nixpkgs.
+                  # With this entry in $NIX_PATH it is possible (and
+                  # recommended) to remove the `nixos` channel for both users
+                  # and root e.g. `nix-channel --remove nixos`. `nix-channel
+                  # --list` should be empty for all users afterwards
+                  nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+                }
+                baseCfg
+                home-manager.nixosModules.home-manager
+                # DONT set useGlobalPackages! It's not necessary in newer
+                # home-manager versions and does not work with configs using
+                # `nixpkgs.config`
+                { home-manager.useUserPackages = true; }
+              ];
+              # Let 'nixos-version --json' know the Git revision of this flake.
+              system.configurationRevision =
+                nixpkgs.lib.mkIf (self ? rev) self.rev;
+              nix.registry.nixpkgs.flake = nixpkgs;
+            })
+          ];
+        };
+    in {
+      nixosConfigurations = {
+        inherit nixpkgs;
+        dell-laptop = defFlakeSystem {
+          imports = [
+            (./hosts/dell-laptop/default.nix)
+            # Add home-manager config
+            { home-manager.users.hub = hub-home.nixosModules.desktop; }
+          ];
+        };
+      };
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
     };
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
 }
